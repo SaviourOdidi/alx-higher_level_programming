2e078b125afeb5a9c7d1d5235eae87d700060fee
Saviour Odidi <saviourdavidodidi@gmail.com>
2023-01-09 15:06:28 -0800

    7. Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 00:08 2023
+@author: Saviour Odidi
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+
+        Raises:
+        Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates integer input
+        """
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        elif value <= 0:
+            raise ValueError(name + " must be greater than 0")
