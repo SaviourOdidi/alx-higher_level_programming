9164fae2deb6a59769f0de306b0eb840b1d4fca5
Saviour Odidi <saviourdavidodidi@gmail.com>
2023-01-09 14:24:01 -0800

    5. Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 09 23:18 2023
+@author: Saviour Odidi
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
