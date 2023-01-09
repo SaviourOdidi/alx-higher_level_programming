b7e90e09620dfddb354279410ab117cbd66b7750
Saviour Odidi <saviourdavidodidi@gmail.com>
2023-01-09 14:28:58 -0800

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 09 23:26 2023
+@author: Saviour Odidi
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+        Raises:
+
+        Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
