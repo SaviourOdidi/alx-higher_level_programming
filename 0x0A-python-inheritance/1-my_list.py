44634530ec819c0ef615a2506f0bb245c7b70be2
Saviour Odidi <saviourdavidodidi@gmail.com>
2023-01-09 16:36:12 -0800

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 09 22:24 2023
-@author: Saviour Odidi
-"""
+# 1-my_list.py
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """
-    class MyList that inherits from list
-    """
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
-    """
-    Public instance method that prints sorted list
-    """
-    list_copy = self[:]
-    list_copy.sort()
-    print(list_copy)
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
